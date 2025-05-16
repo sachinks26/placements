6ea6bdc1a076baf522b28179afb205825a2711fe
sachinks26 <k.s.sachin2005@gmail.com>
2025-05-16 12:03:01 +0530

    Add files via upload

---
+++ amstrong3 .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+ int x=3,y=5;
+ for(int i=1;i<=x;i++){
+     for (int j=1;j<=y;j++){
+         printf("%d",j);
+     }
+     printf("\n");
+ } 
+
+    return 0;
+}
