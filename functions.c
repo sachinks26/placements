fcc5e2a33578f09ebbfe3cf47aed35f1ae6310db
sachinks26 <k.s.sachin2005@gmail.com>
2025-05-20 13:39:10 +0530

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+//return_type funcname();//declaration
+//datatype var_name;
+int add();
+int main()
+{
+   int a=4,b=int6;
+   if(a>b) print("%d",a);
+   else printf("%d",b);
+   //100 lines -------
+   int x=7,y=8;
+   if (x>y)printf("%d",x);
+   else printf("%d",y);
+}
